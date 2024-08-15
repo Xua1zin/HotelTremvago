7fd2ed3cbea0d87ca042957a0ea685f0047e01a9
João Albieri <130722417+Xua1zin@users.noreply.github.com>
2024-08-15 09:25:10 -0300

    Projeto base add

---
+++ src/test/java/com/HotelTremvago/HotelTremvago/HotelTremvagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HotelTremvago.HotelTremvago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelTremvagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
